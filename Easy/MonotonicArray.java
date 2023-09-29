0e5dd35ca2e79bc24c0c22494d1a0b2eb3287ca5
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-09-29 12:04:55 +0530

    Add files via upload

---
+++ Easy/MonotonicArray.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/monotonic-array/
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc=true;
+        boolean dec=true;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                inc=false;
+            }
+            else if(nums[i]>nums[i-1]){
+                dec=false;
+            }
+            if(!inc&&!dec){
+                return false;
+            }
+        }
+        return true;
+    }
+}
